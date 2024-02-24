ec49d922b800fca29fba87f9e438a125564e9f1a
destonkmuo <110706572+destonkmuo@users.noreply.github.com>
2024-02-24 10:56:45 -0600

    fixed android platform bug

---
+++ lib/Pages/loginPage.dart
@@ -0,0 +1,102 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:pickup/components/myButtons.dart';
+import 'package:pickup/components/my_textFields.dart';
+
+class LoginPage extends StatelessWidget {
+  LoginPage({super.key});
+
+  // text editing
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  Future<String?> signUserUp() async {
+    try {
+      String email = usernameController.text;
+
+      if (email.endsWith('@utdallas.edu')) {
+        await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: email,
+          password: passwordController.text,
+        );
+        return null;
+      } else {
+        return 'Email domain is not valid';
+      }
+    } catch (e) {
+      return e.toString(); // Return error message if unsuccessful
+    }
+  }
+
+  Future<String?> signUserIn() async {
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: usernameController.text,
+        password: passwordController.text,
+      );
+      return null;
+    } catch (e) {
+      return e.toString(); // Return error message if unsuccessful
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[500],
+      body: Column(
+        children: [
+          const SizedBox(height: 50),
+          const Icon(
+            Icons.lock,
+            size: 100,
+          ),
+          const SizedBox(height: 50),
+          Text(
+            'Welcome Back to PickUp! Let the games Begin!',
+            style: TextStyle(
+              color: Colors.blue[400],
+              fontSize: 18,
+            ),
+          ),
+          const SizedBox(height: 25),
+          MyTextField(
+            controller: usernameController,
+            hintTxt: 'username',
+            obscureTxt: false,
+          ),
+          const SizedBox(height: 25),
+          MyTextField(
+            controller: passwordController,
+            hintTxt: 'password',
+            obscureTxt: true,
+          ),
+          const SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    // Add your forgot password logic here
+                  },
+                  child: Text(
+                    'Forgot Password?',
+                    style: TextStyle(color: Colors.blue[200]),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          MyButton(
+            onTap: () {
+              signUserIn();
+            },
+          )
+        ],
+      ),
+    );
+  }
+}

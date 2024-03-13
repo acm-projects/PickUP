ef0e4da77819b7cc56abb9895c1fae9d7ec05166
Taaha Hussain <taahahussain@Taahas-MBP.attlocal.net>
2024-03-13 16:49:23 -0500

    Added maps display

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:pickup/Functions/liveMap.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,7 +33,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: LiveMap(),
     );
   }
 }

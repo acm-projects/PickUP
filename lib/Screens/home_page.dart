f87d99f031e9994715b2fcdf7872d39ca9ac33a2
Isha Rao <raoishall@gmail.com>
2024-04-14 17:22:30 -0500

    change colors

--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -43,7 +43,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFF1A3E2F),
+      backgroundColor: const Color(0xFF0C2219),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,

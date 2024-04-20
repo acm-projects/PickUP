e1276bec1a98956d70afed4920e3cac6f822fea6
ahmedsid5086 <ahmed.sidschool2004@gmail.com>
2024-04-20 16:14:13 -0500

    Added Tennis to Stats Page

--- lib/Screens/stats_page.dart
+++ lib/Screens/stats_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
 class StatsPage extends StatefulWidget {
-  const StatsPage({super.key});
-
   @override
   _StatsPageState createState() => _StatsPageState();
 }
@@ -19,11 +17,22 @@ class _StatsPageState extends State<StatsPage> {
       'score': '98-102',
       'result': 'Loss'
     },
+    {'team1': 'Heat', 'team2': 'Knicks', 'score': '109-101', 'result': 'Win'},
+    {
+      'team1': 'Raptors',
+      'team2': 'Clippers',
+      'score': '94-96',
+      'result': 'Loss'
+    },
+    {'team1': 'Bucks', 'team2': 'Nets', 'score': '120-115', 'result': 'Win'},
   ];
 
   List<Map<String, dynamic>> volleyballStats = [
     {'team1': 'Team A', 'team2': 'Team B', 'score': '3-1', 'result': 'Win'},
     {'team1': 'Team C', 'team2': 'Team D', 'score': '2-3', 'result': 'Loss'},
+    {'team1': 'Team X', 'team2': 'Team Y', 'score': '3-0', 'result': 'Win'},
+    {'team1': 'Team P', 'team2': 'Team Q', 'score': '1-3', 'result': 'Loss'},
+    {'team1': 'Team M', 'team2': 'Team N', 'score': '3-2', 'result': 'Win'},
   ];
 
   List<Map<String, dynamic>> soccerStats = [
@@ -38,6 +47,47 @@ class _StatsPageState extends State<StatsPage> {
       'team2': 'Liverpool',
       'score': '0-2',
       'result': 'Loss'
+    },
+    {'team1': 'Arsenal', 'team2': 'Chelsea', 'score': '3-2', 'result': 'Win'},
+    {
+      'team1': 'Bayern Munich',
+      'team2': 'Dortmund',
+      'score': '1-1',
+      'result': 'Draw'
+    },
+    {'team1': 'Juventus', 'team2': 'AC Milan', 'score': '2-0', 'result': 'Win'},
+  ];
+
+  List<Map<String, dynamic>> tennisStats = [
+    {
+      'player1': 'Federer',
+      'player2': 'Nadal',
+      'score': '6-4, 3-6, 6-3',
+      'result': 'Win'
+    },
+    {
+      'player1': 'Djokovic',
+      'player2': 'Murray',
+      'score': '7-6, 4-6, 6-3',
+      'result': 'Win'
+    },
+    {
+      'player1': 'Nadal',
+      'player2': 'Djokovic',
+      'score': '6-3, 3-6, 7-6',
+      'result': 'Win'
+    },
+    {
+      'player1': 'Murray',
+      'player2': 'Federer',
+      'score': '6-2, 4-6, 6-3',
+      'result': 'Win'
+    },
+    {
+      'player1': 'Nadal',
+      'player2': 'Federer',
+      'score': '6-4, 6-3',
+      'result': 'Win'
     },
   ];
 
@@ -72,57 +122,68 @@ class _StatsPageState extends State<StatsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFF0C2219),
-      appBar: AppBar(
-        title: const Text('Stats'),
-        centerTitle: true,
-        titleTextStyle: const TextStyle(
-          color: Colors.black,
-          fontFamily: 'Mada',
-          fontWeight: FontWeight.bold,
-          fontSize: 30,
-        ),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.of(context)
-                .pop(); // Navigates back to the previous screen
-          },
-        ),
-        backgroundColor:
-            Colors.transparent, // Make AppBar background transparent
-        elevation: 0, // Removes shadow
-        flexibleSpace: Ink(
-          decoration: BoxDecoration(
-            gradient: const LinearGradient(
-              colors: [Color(0xFF80F37F), Color(0xFF80E046)], // Gradient colors
-              begin: Alignment.topCenter, // Start point of the gradient
-              end: Alignment.bottomCenter, // End point of the gradient
-            ),
-            borderRadius: BorderRadius.circular(30), // Rounded corners
-          ),
-        ),
-      ),
+      backgroundColor: const Color(0xFF1A3E2F),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          const SizedBox(height: 40),
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color(0xFF80F37F),
+                  Color(0xFF80E046)
+                ], // Gradient colors
+                begin: Alignment.topCenter, // Start point of the gradient
+                end: Alignment.bottomCenter, // End point of the gradient
+              ),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+              ),
+            ),
+            padding: EdgeInsets.symmetric(vertical: 5),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.arrow_back),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                Expanded(
+                  child: Center(
+                    child: Text(
+                      'Stats',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 40), // Adjust spacing as needed
+              ],
+            ),
+          ),
+          SizedBox(height: 20),
           // Dropdown button for selecting the sport
           Center(
             child: Container(
               width: MediaQuery.of(context).size.width *
                   0.5, // Adjust the width here
-              padding: const EdgeInsets.symmetric(horizontal: 20),
+              padding: EdgeInsets.symmetric(horizontal: 20),
               decoration: BoxDecoration(
                 color: Colors.grey[200], // Light grey background color
                 borderRadius: BorderRadius.circular(30),
               ),
               child: DropdownButton<String>(
                 value: selectedSport,
-                icon: const Icon(Icons.arrow_drop_down),
+                icon: Icon(Icons.arrow_drop_down),
                 iconSize: 24,
                 elevation: 16,
-                style: const TextStyle(color: Colors.black, fontSize: 18),
+                style: TextStyle(color: Colors.black, fontSize: 18),
                 underline: Container(
                   height: 0,
                   color: Colors.transparent,
@@ -138,11 +199,13 @@ class _StatsPageState extends State<StatsPage> {
                         stats = volleyballStats;
                       } else if (newValue == 'Soccer') {
                         stats = soccerStats;
+                      } else if (newValue == 'Tennis') {
+                        stats = tennisStats;
                       }
                     });
                   }
                 },
-                items: <String>['Basketball', 'Volleyball', 'Soccer']
+                items: <String>['Basketball', 'Volleyball', 'Soccer', 'Tennis']
                     .map<DropdownMenuItem<String>>((String value) {
                   return DropdownMenuItem<String>(
                     value: value,
@@ -150,18 +213,20 @@ class _StatsPageState extends State<StatsPage> {
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          const SizedBox(width: 10), // Add left padding here
+                          SizedBox(width: 10), // Add left padding here
                           // Display sport icon (you can replace the icons with your own)
                           if (value == 'Basketball')
-                            const Icon(Icons.sports_basketball, color: Colors.black),
+                            Icon(Icons.sports_basketball, color: Colors.black),
                           if (value == 'Volleyball')
-                            const Icon(Icons.sports_volleyball, color: Colors.black),
+                            Icon(Icons.sports_volleyball, color: Colors.black),
                           if (value == 'Soccer')
-                            const Icon(Icons.sports_soccer, color: Colors.black),
-                          const SizedBox(
+                            Icon(Icons.sports_soccer, color: Colors.black),
+                          if (value == 'Tennis')
+                            Icon(Icons.sports_tennis, color: Colors.black),
+                          SizedBox(
                               width: 10), // Add spacing between icon and text
                           Text(value,
-                              style: const TextStyle(
+                              style: TextStyle(
                                   color:
                                       Colors.black)), // Adjust the text color
                         ],
@@ -172,17 +237,17 @@ class _StatsPageState extends State<StatsPage> {
               ),
             ),
           ),
-          const SizedBox(height: 20),
+          SizedBox(height: 20),
           // Wins and Losses Section
           Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
+            padding: EdgeInsets.symmetric(horizontal: 20),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Expanded(
                   child: Column(
                     children: [
-                      const Text(
+                      Text(
                         'Wins',
                         style: TextStyle(
                           fontSize: 24,
@@ -190,10 +255,10 @@ class _StatsPageState extends State<StatsPage> {
                           color: Colors.white,
                         ),
                       ),
-                      const SizedBox(height: 10),
+                      SizedBox(height: 10),
                       Text(
                         '${calculateWins(stats)}', // Calculate wins dynamically
-                        style: const TextStyle(
+                        style: TextStyle(
                           fontSize: 80,
                           fontWeight: FontWeight.bold,
                           color: Colors.white,
@@ -210,7 +275,7 @@ class _StatsPageState extends State<StatsPage> {
                 Expanded(
                   child: Column(
                     children: [
-                      const Text(
+                      Text(
                         'Losses',
                         style: TextStyle(
                           fontSize: 24,
@@ -218,10 +283,10 @@ class _StatsPageState extends State<StatsPage> {
                           color: Colors.white,
                         ),
                       ),
-                      const SizedBox(height: 10),
+                      SizedBox(height: 10),
                       Text(
                         '${calculateLosses(stats)}', // Calculate losses dynamically
-                        style: const TextStyle(
+                        style: TextStyle(
                           fontSize: 80,
                           fontWeight: FontWeight.bold,
                           color: Colors.white,
@@ -235,11 +300,11 @@ class _StatsPageState extends State<StatsPage> {
           ),
           // Recent Games Section
           Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
+            padding: EdgeInsets.symmetric(horizontal: 20),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                const SizedBox(height: 10),
+                SizedBox(height: 10),
                 Row(
                   children: [
                     Expanded(
@@ -248,8 +313,8 @@ class _StatsPageState extends State<StatsPage> {
                         color: Colors.white.withOpacity(0.29),
                       ),
                     ),
-                    const SizedBox(width: 10),
-                    const Text(
+                    SizedBox(width: 10),
+                    Text(
                       'Recent Games',
                       style: TextStyle(
                         fontSize: 25,
@@ -257,7 +322,7 @@ class _StatsPageState extends State<StatsPage> {
                         color: Colors.white,
                       ),
                     ),
-                    const SizedBox(width: 10),
+                    SizedBox(width: 10),
                     Expanded(
                       child: Container(
                         height: 2,
@@ -269,7 +334,7 @@ class _StatsPageState extends State<StatsPage> {
               ],
             ),
           ),
-          const SizedBox(height: 10),
+          SizedBox(height: 10),
           Expanded(
             child: ListView.builder(
               itemCount:
@@ -278,28 +343,28 @@ class _StatsPageState extends State<StatsPage> {
                 // Example game data, replace with actual game data
                 Map<String, dynamic> game = stats[index];
                 return Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
                   color: index.isEven
-                      ? const Color(0xFF255035) // Alternate row color
+                      ? Color(0xFF255035) // Alternate row color
                       : Colors.transparent,
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Text(
                         '${game['team1']} vs ${game['team2']}',
-                        style: const TextStyle(
+                        style: TextStyle(
                           fontSize: 16,
                           color: Colors.white,
                         ),
                       ),
                       Text(
                         '${game['score']}',
-                        style: const TextStyle(
+                        style: TextStyle(
                           fontSize: 16,
                           color: Colors.white,
                         ),
                       ),
-                      const SizedBox(width: 10),
+                      SizedBox(width: 10),
                       Container(
                         width: 60,
                         height: 30,
@@ -307,12 +372,14 @@ class _StatsPageState extends State<StatsPage> {
                         decoration: BoxDecoration(
                           color: game['result'] == 'Win'
                               ? Colors.green
-                              : Colors.red,
+                              : (game['result'] == 'Loss'
+                                  ? Colors.red
+                                  : Colors.blue),
                           borderRadius: BorderRadius.circular(5),
                         ),
                         child: Text(
                           game['result'],
-                          style: const TextStyle(
+                          style: TextStyle(
                             fontSize: 16,
                             fontWeight: FontWeight.bold,
                             color: Colors.white,
